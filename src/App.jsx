128a12273a1c7158fb2d5e2f18fe8ba0852d1b2f
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-11-11 11:38:24 +0100

    add list on page with map

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 function App() {
   const movies = [
-    { title: "Inception", genre: "Fantascienza" },
-    { title: "Il Padrino", genre: "Thriller" },
-    { title: "Titanic", genre: "Romantico" },
-    { title: "Batman", genre: "Azione" },
-    { title: "Interstellar", genre: "Fantascienza" },
-    { title: "Pulp Fiction", genre: "Thriller" },
+    { id: 1, title: "Inception", genre: "Fantascienza" },
+    { id: 2, title: "Il Padrino", genre: "Thriller" },
+    { id: 3, title: "Titanic", genre: "Romantico" },
+    { id: 4, title: "Batman", genre: "Azione" },
+    { id: 5, title: "Interstellar", genre: "Fantascienza" },
+    { id: 6, title: "Pulp Fiction", genre: "Thriller" },
   ];
 
   return (
     <>
-
+      <div className="container">
+        <h1 className="text-white text-center my-3">🎬 Lista Film</h1>
+        <ul class="list-group">
+          {
+            movies.map(item =>
+              <li class="list-group-item bg-light rounded-0" key={item.id}>
+                {item.title} – <em className="text-secondary">{item.genre}</em>
+              </li>
+            )
+          }
+        </ul>
+      </div>
     </>
   )
 }
