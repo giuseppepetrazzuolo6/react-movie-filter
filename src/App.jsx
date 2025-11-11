d76de755efbf141238694b1b8d6a00e6e256c663
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-11-11 11:23:08 +0100

    add movie array

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 function App() {
-
+  const movies = [
+    { title: "Inception", genre: "Fantascienza" },
+    { title: "Il Padrino", genre: "Thriller" },
+    { title: "Titanic", genre: "Romantico" },
+    { title: "Batman", genre: "Azione" },
+    { title: "Interstellar", genre: "Fantascienza" },
+    { title: "Pulp Fiction", genre: "Thriller" },
+  ];
 
   return (
     <>
